3992491b11e917ec04ac85488958d501699963bf
marcosalexandria <marcos.alexandria01@gmail.com>
2023-02-10 11:32:31 -0300

    Collectors.joining()

--- src/main.java
+++ src/main.java
@@ -7,17 +7,17 @@ public class main {
 	public static void main(String[] args) {
 		List<Integer> lista = Arrays.asList(3, 4, 1 ,3 ,4, 6, 1 , 4);
 		
-		List<Integer> listInt = lista.stream()
+		String collectSt = lista.stream()
 		 .skip(2)	//skip ignora uma quantidade de dados do stream (vai ignorar os 2 primeiros registros)
 		 //.limit(1)	//define o limite de dados a serem processados (vai processar só o primeiro registro)
 		 .distinct() // não permite o processamento de dados repitidos (hashcode equals)
 		 .filter(e -> e % 2 == 0)	//um filtro mais especifico
-		 .map(e -> e * 2) //o mep não é um filtro como os de cime, ele é para modificar os registros *não modifica a lista original
+		 .map(e -> String.valueOf(e)) //o mep não é um filtro como os de cime, ele é para modificar os registros *não modifica a lista original
+		 // ^ transformei os valores para string
 		 
-		 
-		 .collect(Collectors.toList()); //o collect é para um retorno mais personalizado, aqui está retornando uma lista de inteiros
+		 .collect(Collectors.joining()); // joining retorna uma string com os registros cocatenados
 		
-		System.out.println(listInt);
+		System.out.println(collectSt);
 	}
 
 }
