9beed47b49a29bd98524da0dfd09fde2e03d18a2
marcosalexandria <marcos.alexandria01@gmail.com>
2023-02-10 10:34:46 -0300

    STREAMS Operações intermediarias
    REDUZA uso de 'for', use STREAMS e simplifique seu código!

--- src/main.java
+++ src/main.java
@@ -6,7 +6,13 @@ public class main {
 	public static void main(String[] args) {
 		List<Integer> lista = Arrays.asList(3, 4, 1 ,3 ,4, 6, 1 , 4);
 		
-		
+		lista.stream()
+		 .skip(2)	//skip ignora uma quantidade de dados do stream (vai ignorar os 2 primeiros registros)
+		 //.limit(1)	//define o limite de dados a serem processados (vai processar só o primeiro registro)
+		 .distinct() // não permite o processamento de dados repitidos (hashcode equals)
+		 .filter(e -> e % 2 == 0)	//um filtro mais especifico
+		 .map(e -> e * 2) //o mep não é um filtro como os de cime, ele é para modificar os registros
+		 .forEach(System.out::println);
 
 	}
 
