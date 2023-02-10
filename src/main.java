73b4acade597617f6cd0c8f005d2be5287feb761
marcosalexandria <marcos.alexandria01@gmail.com>
2023-02-10 14:34:54 -0300

    Optional ifPresent
    Pare de retornar NULL, use OPTIONAL do JAVA 8!

--- src/main.java
+++ src/main.java
@@ -1,23 +1,23 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class main {
 
 	public static void main(String[] args) {
-		List<Integer> lista = Arrays.asList(3, 4, 1 ,3 ,4, 6, 1 , 4);
 		
-		String collectSt = lista.stream()
-		 .skip(2)	//skip ignora uma quantidade de dados do stream (vai ignorar os 2 primeiros registros)
-		 //.limit(1)	//define o limite de dados a serem processados (vai processar só o primeiro registro)
-		 .distinct() // não permite o processamento de dados repitidos (hashcode equals)
-		 .filter(e -> e % 2 == 0)	//um filtro mais especifico
-		 .map(e -> String.valueOf(e)) //o mep não é um filtro como os de cime, ele é para modificar os registros *não modifica a lista original
-		 // ^ transformei os valores para string
-		 
-		 .collect(Collectors.joining()); // joining retorna uma string com os registros cocatenados
+		String s = "1";
+		Optional<Integer> numero = converteEmNumero(s);
+		numero.ifPresent(n -> System.out.println(n));
 		
-		System.out.println(collectSt);
 	}
-
+	
+	public static Optional<Integer> converteEmNumero(String numeroStr) {
+		try {
+			
+			Integer integer = Integer.valueOf(numeroStr);
+			return Optional.of(integer);
+			
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+	}
 }
