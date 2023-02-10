e858dfb46f1eeaf133a7e863eeff7503fb8df6c1
marcosalexandria <marcos.alexandria01@gmail.com>
2023-02-10 10:08:59 -0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) {
+		List<Integer> lista = Arrays.asList(3, 4, 1 ,3 ,4, 6, 1 , 4);
+		
+		
+
+	}
+
+}
